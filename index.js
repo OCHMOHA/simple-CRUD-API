74e6e32d7027a46a6eeb4cb2a7201b6535987885
OCHMOHA <1132245115@pfur.ru>
2025-10-18 16:28:42 +0100

    added form post

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Product = require("./models/product.model.js");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
   res.send("whats up bitch hh");
@@ -30,7 +31,7 @@ app.get("/api/product/:id", async (req, res) => {
 
 app.post("/api/products", async (req, res) => {
   try {
-    const product = await product.create(req.body);
+    const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
